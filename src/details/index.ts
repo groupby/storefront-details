cf22182a605658979bb16952e20c498265825674
Ben Teichman <ben.l.teichman@gmail.com>
2017-05-31 14:49:52 -0400

    simplify tags

--- src/details/index.ts
+++ src/details/index.ts
@@ -1,17 +1,19 @@
-import { view, Component, Events, ProductTransformer, Store, Structure } from '@storefront/core';
+import { tag, Events, ProductTransformer, Store, Structure, Tag } from '@storefront/core';
 
-@view('gb-details', require('./index.html'))
-class Details extends Component {
+@tag('gb-details', require('./index.html'))
+class Details {
+
   structure: Structure = this.config.structure;
   product: Store.Product;
 
-  constructor() {
-    super();
+  init() {
     this.flux.on(Events.DETAILS_PRODUCT_UPDATED, this.updateProduct);
   }
 
   updateProduct = (product: Store.Product) =>
     this.update({ product: ProductTransformer.transform(product, this.structure) })
 }
+
+interface Details extends Tag { }
 
 export default Details;
--- test/unit/details.ts
+++ test/unit/details.ts
@@ -1,7 +1,28 @@
-import { Component } from '@storefront/core';
+import { Events } from '@storefront/core';
 import Details from '../../src/details';
 import suite from './_suite';
 
 suite('Details', ({ expect, spy }) => {
+  let details: Details;
 
+  beforeEach(() => {
+    Details.prototype.config = <any>{};
+    details = new Details();
+  });
+  afterEach(() => delete Details.prototype.config);
+
+  describe('init()', () => {
+    it('should listen for DETAILS_PRODUCT_UPDATED', () => {
+      const on = spy();
+      details.flux = <any>{ on };
+
+      details.init();
+
+      expect(on.calledWith(Events.DETAILS_PRODUCT_UPDATED, details.updateProduct));
+    });
+  });
+
+  describe('updateProduct()', () => {
+    it('should update product');
+  });
 });
