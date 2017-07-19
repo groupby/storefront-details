5e591decb38b9e3f1ec683295a17fca6b9d0b997
Victoria Johns <victoria.johns@groupbyinc.com>
2017-07-19 14:54:09 -0400

    Add configurable and aliased

--- test/unit/details.ts
+++ test/unit/details.ts
@@ -4,7 +4,7 @@ import suite from './_suite';
 
 const STRUCTURE = { a: 'b' };
 
-suite('Details', ({ expect, spy, stub }) => {
+suite('Details', ({ expect, spy, stub, configurable, aliased }) => {
   let details: Details;
 
   beforeEach(() => {
@@ -12,6 +12,9 @@ suite('Details', ({ expect, spy, stub }) => {
     details = new Details();
   });
   afterEach(() => delete Details.prototype.config);
+
+  configurable(Details);
+  aliased(Details);
 
   describe('constructor()', () => {
     it('should set initial values', () => {
