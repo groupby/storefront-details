474a294b6a22d424bcf24bcf0412ac5a58e113cc
Bruce Chen <bruce.chen@groupbyinc.com>
2017-11-13 16:28:35 -0500

    Uses coreselectors

--- src/details/index.ts
+++ src/details/index.ts
@@ -1,25 +1,20 @@
-import { alias, configurable, tag, Events,
-         ProductTransformer, Selectors, Store, Structure, Tag } from '@storefront/core';
+import { alias, configurable, tag, CoreSelectors, Events, Store, Structure, Tag } from '@storefront/core';
 
 @configurable
 @alias('details')
 @tag('gb-details', require('./index.html'))
 class Details {
 
-  structure: Structure = this.config.structure;
   product: Store.Product;
 
   init() {
-    const details = this.select(Selectors.details);
-    if (details && details.product) {
-      this.updateProduct(details.product);
-    }
+    this.updateProduct(this.select(CoreSelectors.transformedDetailsProduct));
     this.flux.on(Events.DETAILS_PRODUCT_UPDATED, this.updateProduct);
   }
 
   updateProduct = (product: Store.Product) => {
     if (product) {
-      this.update({ product: ProductTransformer.transform(product, this.structure) });
+      this.update({ product });
     } else {
       this.update({ product: { data: {}, variants: [{}] } });
     }
