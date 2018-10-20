a9dbbdaf79725898864c36699690982acc904870
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2018-10-20 16:40:47 -0400

    Update CHANGELOG before release
    Suppress npm's default 'commit and tag' behaviour. Update CHANGELOG with new version identifier, commit, and tag the release.

--- tools/release.sh
+++ tools/release.sh
@@ -7,4 +7,10 @@ git commit -m "Generate docs" ./docs
 
 release_type="$(sed -n '/## \[Unreleased\]/ s/.*\[\(.*\)\]/\1/p' CHANGELOG.md)"
 
-npm version "$release_type"
+npm version "$release_type" --no-git-tag-version
+new_version="$(sed -n '/"version"/ s/.*"\(.*\)",\{0,1\}/\1/p' package.json)"
+sed -i '' "s/\[Unreleased\][^$]*/[$new_version] - `date +%Y-%m-%d`/" CHANGELOG.md
+
+msg="Release version $new_version"
+git commit -m "$msg" .
+git tag -a "v$new_version" -m "$msg"
