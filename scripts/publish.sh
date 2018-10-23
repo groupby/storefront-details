824b4547f507de814ed6b87f3e086d4c4ef6caff
Johann Tutor <johann.tutor@groupbyinc.com>
2018-10-23 14:23:49 -0400

    Add usage information

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,12 +9,29 @@ die() {
   exit 1
 }
 
+print_usage() {
+  cat <<EOF
+Usage: ${0##*/} [OPTIONS]
+       ${0##*/} -h
+Publishes the current package to npm.
+
+OPTIONS
+EOF
+  sed -n '/^[[:space:]]*###/ s//   /p' "$BASH_SOURCE"
+}
+
 cd "${BASH_SOURCE%/*}/.."
 
-while getopts ":t:" opt; do
+while getopts ":t:h" opt; do
   case "$opt" in
+    ### -t	The npm tag to release to (defaults to "latest").
     t)
       tag="$OPTARG"
+      ;;
+    ### -h	Print this help.
+    h)
+      print_usage
+      exit 0
       ;;
     \?)
       die "Invalid option: -${OPTARG}"
